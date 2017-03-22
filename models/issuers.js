88c25e16b59ba06092e6cf52ad2146455e0cb99a
Victor Ikoro <ikoro.victor@gmail.com>
2017-03-22 21:43:17 +0100

    init

--- models/issuers.js
+++ models/issuers.js
@@ -3,12 +3,25 @@ const config  = require(__dirname +  "/../configurations/config.js" ).config;
 let request = require("request");
 
 
+
 exports.createIssuer =  function (req,res,next)
 {
     let reqBody = req.body;
+    let requiredFields = ["enrollID","issuerID", "issuerCode", "issuerName", "issuerOrganization", "identityTypeCodes"]
     if(!reqBody)
     {
-        res.status(400).json({ message : "Request body cannnot be empty"});
+        res.status(400).json({ message : "Request body cannot be empty"});
+        return;
+    }
+    requiredFields.forEach(function(val){
+        if(!reqBody["val"]){
+            res.status(400).json({ message : `'${val}' is a required field`});
+            return
+        }
+    })
+    if(reqBody.identityTypeCodes.length == 0)
+    {
+         res.status(400).json({ message : "At least one Identity Type Code needed "});
         return;
     }
 
